3bfc14b3a8c729039afae5fba7e180a52e9576fb
m-salakhedenov <m.salakhedenov@gmail.com>
2022-10-24 16:37:30 +0600

    removed TODOs

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Generics {
 
-    //TODO: Refactor Method-1
     public List<List<String>> boxingMethod(String name) {
         List<String> firstList = new ArrayList<>();
         firstList.add(name);
@@ -14,12 +13,10 @@ public class Generics {
         return secondList;
     }
 
-    //TODO: Refactor Method-2
     public <T> T genericMethod(T data) {
         return data;
     }
 
-    //TODO: Refactor Method-3
     public <T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
